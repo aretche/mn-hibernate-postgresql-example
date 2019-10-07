7a6244ab3695fba6bd6886b5866e7be4cab6bf1a
arellanog <arellanog@cent.gov.ar>
2019-10-07 09:59:40 -0300

    Add domain classes

---
+++ src/main/kotlin/example/micronaut/domain/Book.kt
@@ -0,0 +1,20 @@
+package example.micronaut.domain
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "book")
+data class Book(
+        @Id
+        @GeneratedValue
+        val id: Long,
+
+        @Column(name = "name", nullable = false)
+        val name: String,
+
+        @Column(name = "isbn", nullable = false)
+        val isbn: String,
+
+        @ManyToOne
+        val genre: Genre? = null
+)
